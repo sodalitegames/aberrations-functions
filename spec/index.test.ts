b593d6a7b0998f50a50c0e919e58c4a1caaf1321
Talon Durrant <talonmdurrant@gmail.com>
2022-05-27 10:07:44 -0400

    getting basic structure set up

---
+++ spec/index.test.ts
@@ -0,0 +1,5 @@
+import { add } from '../src/index';
+
+test('adds two numbers', () => {
+  expect(add(7, 1)).toBe(8);
+});
---
+++ src/index.ts
@@ -0,0 +1,7 @@
+console.log('From the index file');
+
+export const add = (a: number, b: number): number => {
+  return a + b;
+};
+
+console.log(add(1, 7));
